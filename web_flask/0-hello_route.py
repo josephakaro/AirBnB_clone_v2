104c6167ac6b536efb03514d0413de527424cff8
Joseph Akaro <josephmmakaro@gmail.com>
2024-04-21 21:43:33 +0200

    Task pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
 """
-	This is Flask App that returns the string "Hello HBNB" through the End-Point
+This is Flask App that returns the string "Hello HBNB" through the End-Point
 """
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def hello_HBNB():
-	"""
-	Route Definitation that returns a string "Hello HBNB!' when GET request is call through port 5000
-	"""
-	return "Hello HBNB!"
-
+def hello_hbnb():
+    """
+    Route Definitation that returns a string "Hello HBNB!'
+    when GET request is call through port 5000
+    """
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
-	app.run(host='0.0.0.0', port=5000)
-
+    app.run(host='0.0.0.0', port=5000)
