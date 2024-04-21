753d6f7c271201e55d031aac7bd4d86ac0d2f207
Joseph Akaro <josephmmakaro@gmail.com>
2024-04-21 22:00:23 +0200

    Task pycodestyle

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+This is a multi-route Flask App that return
+Route A: '/'
+Route B: '/HBNB'
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    End-point that displays 'Hello HBNB'
+    """
+    return "Hello HBNB!"
+
+
+@app.rout('/HBNB', strick_slashes=False)
+def hbnb()
+    """
+    End-point that displays "HBNB"
+    """
+    return "HBNB"
+
+
+if __name__ == "__main__"
+    app.run(host='0.0.0.0', port=5000)
+
