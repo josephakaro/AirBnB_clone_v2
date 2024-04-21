76b0b671760f7b8ffe8112fd2d659c1f0e4ff501
Joseph Akaro <josephmmakaro@gmail.com>
2024-04-21 11:58:48 +0200

    Web flask: 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,16 +1,18 @@
 # Module imports
 import json # Responsible for json representation of data
-from flask import Flask, jsonify, request # Import flask, jsonify to parse the json data, and request module
+from flask import Flask
+
+# Import flask, jsonify to parse the json data, and request module
 
 # Initialising the flask App by assigning flask on app variabe
 app = Flask(__name__)
 
 # Basic Route to run flask app
-@app.route("/", methods=['GET'])
+@app.route("/", strict_slashes=False)
 def Greetings():
     return "Hello HBNB!"
 
 if __name__ == '__main__':
   # Disable Debug on Production Ready Server
   # Use .env files or Secrets in GitHub action to protect the app from spoofing
-   app.run(debug=True, port=5000)
+   app.run(host='0.0.0.0', port=5000)
